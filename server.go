697aa2f36ab6511713ee1d04cf942ec97ff63d46
Naoki Horie <naoe.j1fore2p@gmail.com>
2017-07-06 20:01:24 +0900

    firest code commit

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"io"
+
+	"golang.org/x/net/websocket"
+)
+
+func echoHandler(ws *websocket.Conn) {
+	io.Copy(ws, ws)
+}
+
+func main() {
+	//http.Handle("/echo", websocket.Handler(echoHandler))
+	http.HandleFunc("/echo", func(w http.ResponseWriter, req *http.Request) {
+		s := websocket.Server{Handler: websocket.Handler(echoHandler)}
+		s.ServeHTTP(w, req)
+	})
+	http.Handle("/", http.FileServer(http.Dir("/")))
+
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
